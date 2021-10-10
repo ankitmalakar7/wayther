62b4f457c1974425f54b27db8c11d2350eb9b399
ankitmalakar7 <malamalakar786@gmail.com>
2021-10-10 12:57:46 +0530

    contacts updated

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,24 @@
-import React from 'react'
-import Footer from './Footer'
+import React from "react";
+import Footer from "./Footer";
 
 export default function Contact() {
-    return (
-        <>
-        <div className="container">
+  return (
+    <>
+      <div className="container m">
+        <div className="thb text-center">
+          <p>I'm Ankit Malakar, a CSE student of RCCIIT, Kolkata...</p>
+          <p>
+          For more details you may visit my github profile. My github id is given below.
+          </p>
+          <p>Thank you.</p>
+        </div>
         <div className="sm">
-            <a href="https://github.com/ankitmalakar7"><i class="fab fa-github-square"></i></a>
+          <a href="https://github.com/ankitmalakar7">
+            <i class="fab fa-github-square"></i>
+          </a>
         </div>
-        <Footer/>
-        </div>
-        </>
-    )
+        <Footer />
+      </div>
+    </>
+  );
 }
--- src/components/Features.js
+++ src/components/Features.js
@@ -1,22 +1,18 @@
-import React from 'react'
-import Footer from './Footer'
+import React from "react";
+import Footer from "./Footer";
 
 export default function Features(props) {
-    return (
-        <>
-        <div className="container">
-        <div className="thb" >
-            <li>
-                <ul>
-                    <p>Current & forecasted weather data for any valid location.</p>
-                    <p>Warning of wrong data for every invalid location.</p>
-                    <p>Forcasted weather datas for consecutive 8 days.</p>
-                    <p>Rest ? use & invent...</p>
-                </ul>
-            </li>
+  return (
+    <>
+      <div className="container">
+        <div className="thb text-center">
+          <p>Current & forecasted weather data for any valid location.</p>
+          <p>Warning of wrong data for every invalid location.</p>
+          <p>Forcasted weather datas for consecutive 8 days.</p>
+          <p>Rest ? use & invent...</p>
         </div>
-        <Footer/>
-        </div>
-        </>
-    )
+        <Footer />
+      </div>
+    </>
+  );
 }
